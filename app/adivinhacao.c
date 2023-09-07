7206f60d026602434fd57794912981ddde7ca347
magedeungaro <magedbgt@gmail.com>
2023-09-07 19:48:03 -0300

    chapter 4 com um toque de Mage

--- app/adivinhacao.c
+++ app/adivinhacao.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 void print_welcome() {
   printf("\n");
@@ -11,6 +12,10 @@ int generate_secret_number() {
   return 42;
 }
 
+int set_tries() {
+  return 3;
+}
+
 int get_guess() {
   int guess;
   printf("Qual é o seu chute? ");
@@ -19,9 +24,10 @@ int get_guess() {
   return guess;
 }
 
-void print_result(const int guess, const int secret_number) {
+int analyze_result(const int guess, const int secret_number) {
   if(guess == secret_number) {
     printf("Parabéns! Você acertou!\n");
+    return true;
   }
   else if (guess > secret_number) {
     printf("Seu chute foi maior do que o número secreto!\n");
@@ -29,13 +35,28 @@ void print_result(const int guess, const int secret_number) {
   else {
     printf("Seu chute foi menor do que o número secreto!\n");
   }
+
+  return false;
+}
+
+void run_game(int tries, const int secret_number) {
+  int guessed_right = false;
+
+  for (int i = 0; !guessed_right && i < tries; i++) {
+    int current_try = i + 1;
+    int has_tries = current_try < tries;
+    int guess = get_guess();
+    printf("Tentativa %d de %d\n", current_try, tries);
+    guessed_right = analyze_result(guess, secret_number);
+  }
 }
 
 int main() {
   print_welcome();
   int secret_number = generate_secret_number();
-  int guess = get_guess();
-  print_result(guess, secret_number);
+  int tries = set_tries();
+  run_game(tries, secret_number);
+  printf("Obrigada por jogar!\n");
 
   return 0;
 }
