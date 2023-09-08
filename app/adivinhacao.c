6c30c3581b8943c1070ff410cf2981f1cb0e0b45
magedeungaro <magedbgt@gmail.com>
2023-09-07 23:18:02 -0300

    difficulty level

--- app/adivinhacao.c
+++ app/adivinhacao.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
+#include <stdlib.h>
+#include <time.h>
 
+typedef struct {
+  int tries;
+  int secret_number;
+  int guess;
+  int current_try;
+} ScoreParams;
+
+int array_includes(const int array[], int element) {
+  int length = *(&array + 1) - array;
+
+  for (int i = 0; i < length; i++) {
+    if (array[i] == element) {
+        return true;
+    }
+  }
+
+  return false;
+}
 
 void print_welcome() {
   printf("\n");
@@ -10,31 +30,65 @@ void print_welcome() {
   printf("************************************\n");
 }
 
+void seed_random() {
+  int seconds = time(NULL);
+  srand(seconds);
+}
+
 int generate_secret_number() {
-  return 42;
+  seed_random();
+  return rand() % 100 + 1;
 }
 
-int set_tries() {
-  return 3;
+int get_dificulty() {
+  int difficulty;
+  int valid_choice = false;
+  int choices[] = {1, 2, 3, 12};
+
+  do {
+    printf("Qual a dificuldade? \n");
+    printf("1 - Fácil \n");
+    printf("2 - Médio \n");
+    printf("3 - Difícil \n");
+    printf("12 - Impossível \n");
+    scanf("%d", &difficulty);
+    printf("A dificuldade escolhida foi %d\n", difficulty);
+
+    valid_choice = array_includes(choices, difficulty);
+  } while (!valid_choice);
+
+  return difficulty;
 }
 
-int calculate_score(const int current_try) {
-  const int initial_score = 1000;
-  int score = initial_score - ((int)pow(current_try, 3) * 15);
+int set_tries(const int difficulty) {
+  const int max_tries = 24;
+
+  return max_tries / difficulty;
+}
+
+int calculate_score(const ScoreParams score_params) {
+  const int initial_score = 10000;
+  const int try_modifier = ((int)pow(score_params.current_try, 2) * 3);
+  const int number_delta = abs(score_params.secret_number - score_params.guess);
+  int score = initial_score - try_modifier - number_delta - score_params.tries;
   
   return score;
 }
 
 int get_guess() {
   int guess;
-  printf("Qual é o seu chute? ");
-  scanf("%d", &guess);
-  int unacceptable_guess = guess < 0 || guess > 100;
+  int unacceptable_guess = false;
 
-  while (unacceptable_guess) {
-    printf("Chute inválido. Apenas valores entre 0 e 100.\n");
+  do {
+    printf("Qual é o seu chute? ");
     scanf("%d", &guess);
-  }
+    unacceptable_guess = guess < 0 || guess > 100;
+
+    if (unacceptable_guess) {
+      printf("Chute inválido. Apenas valores entre 0 e 100.\n");
+    }
+  } while (unacceptable_guess);
+
   return guess;
 }
 
@@ -56,25 +110,29 @@ int analyze_result(const int guess, const int secret_number) {
 void run_game(int tries, const int secret_number) {
   int guessed_right = false;
   int current_try;
+  int guess;
 
   for (int i = 0; !guessed_right && i < tries; i++) {
     current_try = i + 1;
     int has_tries = current_try < tries;
-    int guess = get_guess();
+
     printf("Tentativa %d de %d\n", current_try, tries);
+    guess = get_guess();
     guessed_right = analyze_result(guess, secret_number);
   }
 
-  if(guessed_right) {
-    int score = calculate_score(current_try);
-    printf("Seu score foi de %d\n", score);
-  }
+  printf("O número secreto era %d\n", secret_number);
+  ScoreParams score_params = {tries, secret_number, guess, current_try};
+  int score = calculate_score(score_params);
+  printf("Seu score foi de %d\n", score);
 }
 
 int main() {
   print_welcome();
   int secret_number = generate_secret_number();
-  int tries = set_tries();
+  printf("Número secreto: %d\n", secret_number);
+  int difficulty = get_dificulty();
+  int tries = set_tries(difficulty);
   run_game(tries, secret_number);
   printf("Obrigada por jogar!\n");
 
